6564e6bc7a82b31fff9f0125c8c3fd9311108e3e
vibhusehra <vibhusehra@gmail.com>
2020-07-27 16:59:46 +0530

    added comments to decode_string

--- decode_song.py
+++ decode_song.py
@@ -10,6 +10,12 @@ received = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
 
 decoded = ""
 for i in range(0, len(received), 8):
+	'''
+		map(str, [int list]): will convert all ints to string: [1,0,1]: ['1','0','1']
+		.join(): convert list to string
+		int(string, 2): convert given string to integer and the given base is 2(binary)
+		chr: convert the given integer to it's corresponding character
+	'''
 	char = chr(int("".join(map(str, received[i:i+8])), 2))
 	decoded += char
 	if end_char in decoded[len(decoded) - len(end_char) - 1:-1]:
