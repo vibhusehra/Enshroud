93a5d296e237af5cc3843cc75941a5edb3a6a26e
vibhusehra <vibhusehra@gmail.com>
2020-08-05 12:34:00 +0530

    Conditions added to receiver GUI

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from Prime import Prime
+from tkinter import messagebox
 
 def addSendTab():
 	tab1.columnconfigure(1, weight=1)
@@ -37,9 +39,53 @@ def load_file():
 		try:
 			# print("""here it comes: """)
 			print(fname)
-		except:                     # <- naked except is a bad idea
+		except:                     
 			showerror("Open Source File", "Failed to read file\n'%s'" % fname)
 		return
+
+def deleteText(entry):
+	entry.delete(0, END)
+
+def generatePrimes(prime1Entry, prime2Entry):
+	(x, y) = Prime.generatePrimePair()
+	# print(x,y)
+	deleteText(prime1Entry)
+	deleteText(prime2Entry)
+	prime1Entry.insert(0, str(x))
+	prime2Entry.insert(0, str(y))
+
+def displayError(text):
+	messagebox.showerror(message=text)
+
+def checkEntries(prime1Entry, prime2Entry):
+	p1 = (prime1Entry.get()).strip()
+	p2 = (prime2Entry.get()).strip()
+	# print(p1.isdigit())
+	# print(p1, p2) 
+	# print(not p1.isdigit())
+	if p1.isdigit() == False or p2.isdigit() == False:
+		displayError("Please enter a valid Integer!!")
+		return
+
+	p1 = int(p1)
+	p2 = int(p2)
+	if Prime.checkPrime(p1) and Prime.checkPrime(p2):
+		# create keys
+		print('Both Primes')
+	else:
+		ls = []
+		if(Prime.checkPrime(p1) == False): 
+			ls.append(p1)
+			deleteText(prime1Entry)
+		if(Prime.checkPrime(p2) == False): 
+			ls.append(p2)
+			deleteText(prime2Entry)
+		message = "Please enter valid prime numbers:\n"
+		for elem in ls:
+			message += "{} is not a prime\n".format(elem)
+		displayError(message)
+
+
 def addReceiveTab():
 	tab2.columnconfigure(1, weight=1)
 	tab2.columnconfigure(0, weight=1)
@@ -54,10 +100,10 @@ def addReceiveTab():
 	prime2Entry = Entry(tab2, width=20, borderwidth=3)
 	prime2Entry.grid(row=1, column=2, padx=20)
 
-	generatePrimeButton = Button(tab2, text="Generate Prime Pair")
+	generatePrimeButton = Button(tab2, text="Generate Prime Pair", command=lambda: generatePrimes(prime1Entry, prime2Entry))
 	generatePrimeButton.grid(row=2, column=1, sticky="ew")
 
-	generateKeyButton = Button(tab2, text="Generate Keys")
+	generateKeyButton = Button(tab2, text="Generate Keys", command=lambda: checkEntries(prime1Entry, prime2Entry))
 	generateKeyButton.grid(row=3, column=1, pady=5, sticky="ew")
 
 	p1 = "123"
